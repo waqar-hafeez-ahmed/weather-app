48cade1b59b2581a602ac745409bc0a6b54a8f57
Waqar-Hafeez-Ahmed <you@example.com>
2024-06-24 23:07:54 +0500

    Error handling

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const searchButton = document.getElementById("search-button");
 const cityInput = document.getElementById("city-input");
-const weatherInfoElement = document.getElementById("weather-info");
+const weatherInfoDiv = document.getElementById("weather-info");
 const searchWeatherDiv = document.getElementById("search-weather");
 const searchIcon = document.getElementById("search-icon");
-searchButton.addEventListener("click", handleSearchButtonClick);
-cityInput.addEventListener("keydown", handleEnterKeyPress);
+const loadingMsg = document.getElementById("loading");
+
+//Adding click events to the event  handlers
+searchButton.addEventListener("click", handleSearchButton);
+cityInput.addEventListener("keydown", handleEnterKey);
 searchIcon.addEventListener("click", () => {
   searchWeatherDiv.classList.remove("search-weather-input");
-  weatherInfoElement.innerHTML = "";
+  weatherInfoDiv.innerHTML = "";
   searchIcon.classList.remove("search-icon-visiblity");
 });
 
-function handleSearchButtonClick() {
-  const city = cityInput.value.trim();
+function handleSearchButton() {
+  const city = cityInput.value;
   if (city) {
     getWeather(city);
+    //Removing the weather title and serach button input div. And adding the search icon at the same time.
     searchWeatherDiv.classList.add("search-weather-input");
     searchIcon.classList.add("search-icon-visiblity");
   } else {
@@ -22,34 +26,53 @@ function handleSearchButtonClick() {
   }
 }
 
-function handleEnterKeyPress(event) {
+//Adding a click event on the key press.
+function handleEnterKey(event) {
   if (event.key === "Enter") {
     searchButton.click();
   }
 }
 
 function getWeather(city) {
-  const cachedWeather = localStorage.getItem(city);
-  if (cachedWeather) {
-    const weather = JSON.parse(cachedWeather);
-    console.log(weather);
+  //fetching data from local storage.
+
+  loadingMsg.style.display = "block";
+  const storedWeather = localStorage.getItem(city);
+  if (storedWeather) {
+    const weather = JSON.parse(storedWeather);
+    // console.log(weather);
+    loadingMsg.style.display = "none";
 
     displayWeather(weather);
   } else {
-    fetchWeatherData(city)
-      .then((weather) => {
-        localStorage.setItem(city, JSON.stringify(weather));
-        console.log(weather);
-        displayWeather(weather);
-      })
-      .catch((error) => console.error(error));
-  }
-}
+    //fetced data incase we don't have data in storage.
 
-function fetchWeatherData(city) {
-  return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bd892be8825eb13293fb64432eec1d34&units=metric`
-  ).then((response) => response.json());
+    fetchWeatherData(city);
+  }
+
+  function fetchWeatherData(city) {
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bd892be8825eb13293fb64432eec1d34&units=metric`
+    )
+      .then((response) => response.json())
+      .then((weather) => {
+        if (weather.cod == 404) {
+          console.log(weather, weather.cod, weather.message);
+          alert(weather.message);
+          loadingMsg.innerHTML = "Search Again";
+          loadingMsg.style.display = "none";
+
+          return;
+        } else {
+          console.log("Hello world");
+          localStorage.setItem(city, JSON.stringify(weather));
+          loadingMsg.style.display = "none";
+
+          displayWeather(weather);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
 }
 
 function displayWeather(weather) {
@@ -80,75 +103,9 @@ function displayWeather(weather) {
     </div>
   `;
 
-  weatherInfoElement.innerHTML = weatherHTML;
+  weatherInfoDiv.innerHTML = weatherHTML;
 }
 
-// document.getElementById("search-button").addEventListener("click", () => {
-//   const city = document.getElementById("city-input").value;
-
-//   if (city) {
-//     getWeather(city);
-//   } else {
-//     alert("Please enter a city name");
-//   }
-// });
-
-// document.getElementById("city-input").addEventListener("keydown", (e) => {
-//   if (e.key === "Enter") {
-//     document.getElementById("search-button").click();
-//   }
-// });
-
-// const getWeather = (city) => {
-//   const storage = localStorage.getItem(city);
-//   if (storage) {
-//     const weatherData = JSON.parse(storage);
-//     console.log("I've data", weatherData);
-//     displayWeather(weatherData);
-//   } else {
-//     console.log("I don't have data");
-//     fetch(
-//       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bd892be8825eb13293fb64432eec1d34&units=metric`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const weatherData = data;
-//         localStorage.setItem(city, JSON.stringify(data));
-//         console.log("Fetching data....", data);
-
-//         displayWeather(weatherData);
-//       })
-//       .catch((error) => console.log(error));
-//   }
-// };
-
-// const displayWeather = (data) => {
-//   const infoDiv = document.getElementById("weather-info");
-//   infoDiv.innerHTML = `
-//   <img src="https://openweathermap.org/img/wn/${
-//     data.weather[0].icon
-//   }@2x.png" width="150px"/>
-
-//   <h2>${data.name}</h2>
-
-//   <p class="temperature"> ${Math.round(data.main.temp)}&#8451;</p>
-// <small>${data.weather[0].main}<small/>
-
-//   <div class="weather-info">
-
-//   <div class="info-container">
-//     <i class="fa-light fa-wind"></i>
-//     <h3>${Math.ceil(data.wind.speed * 3.6)}<span class="sub">km/h<span/></h3>
-//     <p>Wind</p>
-//   </div>
-
-//   <div class="vr"></div>
-
-//   <div class="info-container">
-//     <i class="fa-sharp fa-thin fa-droplet"></i>
-//     <h3>${data.main.humidity}<span class="sub">%<span/></h3>
-//     <p>Humidity</p>
-//   </div>
-
-// </div>`;
-// };
+//how to display error on the screen.
+// Responsive.
+//click event on the search icon.
