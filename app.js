3471600bdc844412ad5830b168138348cbacfc48
Waqar-Hafeez-Ahmed <you@example.com>
2024-06-25 13:59:16 +0500

    Deployment test

--- app.js
+++ app.js
@@ -5,48 +5,47 @@ const searchWeatherDiv = document.getElementById("search-weather");
 const searchIcon = document.getElementById("search-icon");
 const loadingMsg = document.getElementById("loading");
 
-//Adding click events to the event  handlers
-searchButton.addEventListener("click", handleSearchButton);
-cityInput.addEventListener("keydown", handleEnterKey);
-searchIcon.addEventListener("click", () => {
+const handleSearchAgain = () => {
+  // Created a function to reset the app to its initial position.
   searchWeatherDiv.classList.remove("search-weather-input");
   weatherInfoDiv.innerHTML = "";
   searchIcon.classList.remove("search-icon-visiblity");
-});
+  cityInput.value = "";
+};
 
-function handleSearchButton() {
+const handleSearchButton = () => {
   const city = cityInput.value;
-  if (city) {
-    getWeather(city);
-    //Removing the weather title and serach button input div. And adding the search icon at the same time.
-    searchWeatherDiv.classList.add("search-weather-input");
-    searchIcon.classList.add("search-icon-visiblity");
-  } else {
+  if (!city) {
     alert("Please enter a city name");
+    return;
   }
-}
+  getWeather(city);
+  // Removing the weather title and search button input div. And adding the search icon at the same time.
+  searchWeatherDiv.classList.add("search-weather-input");
+  searchIcon.classList.add("search-icon-visiblity");
+};
 
-//Adding a click event on the key press.
-function handleEnterKey(event) {
+// Adding a click event on the key press.
+const handleEnterKey = (event) => {
   if (event.key === "Enter") {
-    searchButton.click();
+    if (event.target === cityInput) {
+      handleSearchButton();
+    } else if (event.target === searchIcon) {
+      handleSearchAgain();
+    }
   }
-}
+};
 
-function getWeather(city) {
-  //fetching data from local storage.
-
+const getWeather = (city) => {
+  // Fetching data from local storage.
   loadingMsg.style.display = "block";
   const storedWeather = localStorage.getItem(city);
   if (storedWeather) {
     const weather = JSON.parse(storedWeather);
-    // console.log(weather);
     loadingMsg.style.display = "none";
-
     displayWeather(weather);
   } else {
-    //fetced data incase we don't have data in storage.
-
+    // Fetch data in case we don't have data in storage.
     fetchWeatherData(city);
   }
 
@@ -57,33 +56,27 @@ function getWeather(city) {
       .then((response) => response.json())
       .then((weather) => {
         if (weather.cod == 404) {
-          console.log(weather, weather.cod, weather.message);
           alert(weather.message);
-          loadingMsg.innerHTML = "Search Again";
           loadingMsg.style.display = "none";
-
+          handleSearchAgain();
           return;
         } else {
-          console.log("Hello world");
           localStorage.setItem(city, JSON.stringify(weather));
           loadingMsg.style.display = "none";
-
           displayWeather(weather);
         }
       })
       .catch((error) => console.log(error));
   }
-}
+};
 
 function displayWeather(weather) {
   const weatherHTML = ` <h2>${weather.name}</h2>
     <img src="https://openweathermap.org/img/wn/${
       weather.weather[0].icon
     }@2x.png" width="150px" alt="Icon"/>
-
     <p class="temperature"> ${Math.round(weather.main.temp)}<sup>o</sup></p>
     <small>${weather.weather[0].main}<small/>
-
     <div class="weather-info">
       <div class="info-container">
         <i class="fa-light fa-wind"></i>
@@ -92,9 +85,7 @@ function displayWeather(weather) {
         )}<span class="sub">km/h<span/></h3>
         <p>Wind</p>
       </div>
-
       <div class="vr"></div>
-
       <div class="info-container">
         <i class="fa-sharp fa-thin fa-droplet"></i>
         <h3>${weather.main.humidity}<span class="sub">%<span/></h3>
@@ -102,10 +93,11 @@ function displayWeather(weather) {
       </div>
     </div>
   `;
-
   weatherInfoDiv.innerHTML = weatherHTML;
 }
 
-//how to display error on the screen.
-// Responsive.
-//click event on the search icon.
+// Adding click events to the event handlers
+searchButton.addEventListener("click", handleSearchButton);
+cityInput.addEventListener("keydown", handleEnterKey);
+searchIcon.addEventListener("click", handleSearchAgain);
+searchIcon.addEventListener("keydown", handleEnterKey);
